23c34c607aca3f905b0704d5dd87aac03a4a5613
KosMoS7NS <104136933+KosMoS7NS@users.noreply.github.com>
2022-09-21 12:55:21 +0200

    UF1
    Added new Exception and reformat code, project done

--- UF1/src/main/java/com/ifp/Main.java
+++ UF1/src/main/java/com/ifp/Main.java
@@ -4,6 +4,9 @@ import javax.crypto.*;
 import java.util.Scanner;
 
 import static java.lang.System.*;
+import static javax.crypto.Cipher.*;
+import static javax.crypto.Cipher.DECRYPT_MODE;
+import static javax.crypto.Cipher.ENCRYPT_MODE;
 
 public class Main {
     public static void main(String[] args) {
@@ -24,45 +27,48 @@ public class Main {
                 out.println("Introduce una respuesta: ");
                 respuesta = scanner.nextInt();
 
-                switch (respuesta) {
-                    case 1:
-                        out.println("Introduce el texto a cifrar: ");
-                        scanner.nextLine();
-                        mensaje = scanner.nextLine();
+                try {
+                    switch (respuesta) {
+                        case 1:
+                            out.println("Introduce el texto a cifrar: ");
+                            scanner.nextLine();
+                            mensaje = scanner.nextLine();
 
-                        secretKey = KeyGenerator
-                                .getInstance("AES")
-                                .generateKey();
+                            secretKey = KeyGenerator
+                                    .getInstance("AES")
+                                    .generateKey();
 
-                        cipher = Cipher.getInstance("AES");
-                        cipher.init(Cipher.ENCRYPT_MODE, secretKey);
-                        cifrado = cipher.doFinal(mensaje.getBytes());
+                            cipher = getInstance("AES");
+                            cipher.init(ENCRYPT_MODE, secretKey);
+                            cifrado = cipher.doFinal(mensaje.getBytes());
 
-                        out.println("|=============================|");
-                        out.println("Mensaje cifrado: " + cifrado);
-                        break;
+                            out.println("|=============================|");
+                            out.println("Mensaje cifrado: " + cifrado);
+                            break;
 
-                    case 2:
-                        cipher.init(Cipher.DECRYPT_MODE, secretKey);
-                        byte[] descifrado = cipher.doFinal(cifrado);
+                        case 2:
+                            cipher.init(DECRYPT_MODE, secretKey);
+                            byte[] descifrado = cipher.doFinal(cifrado);
 
-                        out.println("|=============================|");
-                        out.println("Mensaje descifrado: " + new String(descifrado));
-                        break;
+                            out.println("|=============================|");
+                            out.println("Mensaje descifrado: " + new String(descifrado));
+                            break;
 
-                    case 3:
-                        break;
+                        case 3:
+                            break;
 
-                    default:
-                        out.println("Enter the data correctly");
-                        break;
+                        default:
+                            out.println("Enter the data correctly");
+                            break;
+                    }
+
+                } catch (Exception e) {
+                    out.println("Encrypt the data first");
                 }
-
             }
 
-
         } catch (Exception e) {
-            out.println("Encrypt the data first");
+            out.println("Enter the data correctly");
         }
     }
 }
