0abc85fa648046e211af132dfc75503634fe004b
KosMoS7NS <104136933+KosMoS7NS@users.noreply.github.com>
2022-09-22 17:48:49 +0200

    Crypt DES
    done

--- UF1/src/main/java/com/ifp/Main.java
+++ UF1/src/main/java/com/ifp/Main.java
@@ -1,8 +1,10 @@
 package com.ifp;
 
 import com.ifp.cifradoAES.CifradoAES;
+import com.ifp.cifradoDES.CifradoDES;
 
 import javax.crypto.*;
+import java.io.IOException;
 import java.util.Scanner;
 
 import static java.lang.System.*;
@@ -11,7 +13,23 @@ import static javax.crypto.Cipher.DECRYPT_MODE;
 import static javax.crypto.Cipher.ENCRYPT_MODE;
 
 public class Main {
-    public static void main(String[] args) {
-        CifradoAES.cifradoAES();
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(in);
+        int respuesta = 0;
+
+        while (respuesta != 3) {
+            out.println("|=============================|" + lineSeparator() +
+                    "|1) Cifrado AES               |" + lineSeparator() +
+                    "|2) Cifrado DES               |" + lineSeparator() +
+                    "|3) Salir                     |" + lineSeparator() +
+                    "|=============================|");
+            out.println("Introduce una respuesta: ");
+            respuesta = scanner.nextInt();
+
+            switch (respuesta) {
+                case 1 -> CifradoAES.cifradoAES();
+                case 2 -> CifradoDES.cifradoDES();
+            }
+        }
     }
 }
